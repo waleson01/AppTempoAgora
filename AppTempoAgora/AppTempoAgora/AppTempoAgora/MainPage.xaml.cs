767d5cf354d932012b43eaf8bb6dcabe6448ce29
waleson01 <waleson01@users.noreply.github.com>
2021-03-23 23:02:39 -0300

    Completo OK!

---
+++ AppTempoAgora/AppTempoAgora/AppTempoAgora/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using AppTempoAgora.Model;
+using AppTempoAgora.Services;
+
+namespace AppTempoAgora
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Title = "Previsão do Tempo";
+            this.BindingContext = new Tempo();
+        }
+
+        private async void btnPrevisao_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(cidadeEntry.Text))
+                {
+                    Tempo previsaoDoTempo = await DataService.GetPrevisaoDoTempo(cidadeEntry.Text);
+                    this.BindingContext = previsaoDoTempo;
+                    btnPrevisao.Text = "Nova Previsão";
+                }
+            }
+            catch(Exception ex)
+            {
+                await DisplayAlert("Erro ", ex.Message, "OK");
+            }
+        }
+    }
+}
---
+++ AppTempoAgora/AppTempoAgora/AppTempoAgora/Services/DataService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AppTempoAgora.Model;
+
+namespace AppTempoAgora.Services
+{
+    class DataService
+    {
+        public static async Task<Tempo> GetPrevisaoDoTempo(string cidade)
+        {
+            string appId = "a453bc8d644f3f8448afddb1ce63e023";
+
+            string queryString = "http://api.openweathermap.org/data/2.5/weather?q=" + cidade + "&units=metric" + "&appId=" + appId;
+            dynamic resultado = await getDataFromService(queryString).ConfigureAwait(false);
+
+            if(resultado["weather"] != null)
+            {
+                Tempo previsao = new Tempo();
+                previsao.Title = (string)resultado["name"];
+                previsao.Temperature = (string)resultado["main"]["temp"] + " C";
+                previsao.Wind = (string)resultado["wind"]["speed"] + " mph";
+                previsao.Humidity = (string)resultado["main"]["humidity"] + " %";
+                previsao.Visibility = (string)resultado["weather"][0]["main"];
+                DateTime time = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+                DateTime sunrise = time.AddSeconds((double)resultado["sys"]["sunrise"]);
+                DateTime sunset = time.AddSeconds((double)resultado["sys"]["sunset"]);
+                previsao.Sunrise = String.Format("{0:d/MM/yyyy HH:mm:ss}", sunrise);
+                previsao.Sunset = String.Format("{0:d/MM/yyyy HH:mm:ss}", sunset);
+                return previsao;
+
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static async Task<dynamic> getDataFromService(string queryString)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(queryString);
+            dynamic data = null;
+            if(response != null)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+                data = JsonConvert.DeserializeObject(json);
+            }
+            return data;
+
+        }
+
+        public static async Task<dynamic> getDatafromServiceByCity (string city)
+        {
+            string appId = "a453bc8d644f3f8448afddb1ce63e023";
+
+            string url = string.Format("http://api.openweathermap.org/data/2.5/forecast/daily?q={0}&units=metric&cnt=1&APPID={1}", city.Trim(), appId);
+            
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(url);
+            
+            dynamic data = null;
+            
+            if(response != null)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+                data = JsonConvert.DeserializeObject(json);
+            }
+            return data;
+        }
+
+    }
+}
